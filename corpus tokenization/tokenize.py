fb8aaed7fab4ae9eaa20b82015aa6a8c33354594
Agnes <gaspardagnes@gmail.com>
2020-04-15 15:34:07 -0500

    Get word vectors with initial BERT model.

--- corpus tokenization/tokenize.py
+++ corpus tokenization/tokenize.py
@@ -13,10 +13,14 @@ def tokenize(directory_input, input_file, tokens_occ, tokens_per_file):
    return tokens_occ, tokens_per_file
 
 def writeVocabularyFiles(wiki_filename, sorted_d):
+    nb_sup_10 = 0
     # Write unique wiki words in a file
     wiki_file = open(wiki_filename, 'w', encoding="utf-8")
     for word, occ in sorted_d.items():
-        wiki_file.write(str(word) + " " + str(occ) + "\n")
+        #wiki_file.write(str(word) + " " + str(occ) + "\n")
+        if occ > 10:
+            nb_sup_10 += 1
+    return nb_sup_10
 
 def tokenizeAllFiles(all_input_filenames, tokens_occ, tokens_per_file):
     i = 0
@@ -28,11 +32,12 @@ def tokenizeAllFiles(all_input_filenames, tokens_occ, tokens_per_file):
         i += 1
     return tokens_occ, tokens_per_file
  
-def showResults(sorted_d):
+def showResults(sorted_d, nb_sup_10):
     # Show the most occuring word and number of wiki words   
     print(list(sorted_d.keys())[0], list(sorted_d.values())[0])
     print("\nNumber of wiki words:", len(tokens_occ))       
-
+    print(nb_sup_10)
+    print("Portion:", nb_sup_10/len(tokens_occ))
 
 
   
@@ -48,7 +53,7 @@ all_input_filenames = os.listdir(directory_input)
 
 tokens_occ, tokens_per_file = tokenizeAllFiles(all_input_filenames, tokens_occ, tokens_per_file)
 sorted_d = dict(sorted(tokens_occ.items(), key=operator.itemgetter(1),reverse=True))
-writeVocabularyFiles(wiki_filename, sorted_d)
+nb_sup_10 = writeVocabularyFiles(wiki_filename, sorted_d)
 
-showResults(sorted_d)
+showResults(sorted_d, nb_sup_10)
 
